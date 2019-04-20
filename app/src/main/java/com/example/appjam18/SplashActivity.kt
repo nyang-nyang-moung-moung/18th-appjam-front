39c5f6102e4d57dc6504ed10eeb186788c6516aa
B1ns <qlsl3574@naver.com>
2019-04-21 00:54:40 +0900

    Splash_finish

--- app/src/main/java/com/example/appjam18/SplashActivity.kt
+++ app/src/main/java/com/example/appjam18/SplashActivity.kt
@@ -1,12 +1,20 @@
 package com.example.appjam18
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val hander = Handler()
+        hander.postDelayed({
+            startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+            finish()
+        },3500)
     }
 }
