355d2cad07c672fa68c91f475c47aee404d1ae5d
B1ns <qlsl3574@naver.com>
2019-04-21 00:28:37 +0900

    good

--- app/src/main/java/com/example/appjam18/registerActivity.kt
+++ app/src/main/java/com/example/appjam18/registerActivity.kt
@@ -3,10 +3,10 @@ package com.example.appjam18
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class  MainActivity : AppCompatActivity() {
+class registerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
     }
 }
